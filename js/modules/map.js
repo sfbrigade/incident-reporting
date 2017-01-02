1acef20a514c78372ae4d0b847266408856c7ffc
dtgee <techan88@gmail.com>
2017-01-02 13:09:30 -0800

    try more seamless map sleep approach

--- js/modules/map.js
+++ js/modules/map.js
@@ -51,15 +51,15 @@ var mapModule = (function(window,$){
 				  //time(ms) until map wakes on mouseover
 					wakeTime: 750,
 				  //Defines whether or not the user is prompted on how to wake map
-					sleepNote: true,
+					sleepNote: false,
 				  //Allows ability to override note styling
 					sleepNoteStyle: { color: 'red' },
 				  //Should hovering wake the map? (clicking always will)
-					hoverToWake: false,
+					hoverToWake: true,
 					//A message to inform users of waking map
 					wakeMessage: 'Click to Wake',
 				  //Opacity (between 0 and 1) of inactive map
-					sleepOpacity: .7
+					sleepOpacity: 0 
 				}).setView([37.767806, -122.438153], 12);
 		_components["layers"]["searchradius"] = L.circle([37.767806, -122.438153], 402.3).addTo(_components["map"]);
 
