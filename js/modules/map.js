fceb4ce330dfe7638b1d973883de7b660efe07b5
dtgee <techan88@gmail.com>
2017-01-07 16:13:16 -0800

    commit local changes for branch merge

--- js/modules/map.js
+++ js/modules/map.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 var mapModule = (function(window,$){
 
 	/*Global variables within the module scope*/
@@ -189,6 +190,128 @@ var mapModule = (function(window,$){
   		drawApiResponse:_drawApiResponse,
   		showLoader: _showLoader,
   		hideLoader: _hideLoader
+=======
+var mapModule = (function(window,$) {
+
+    var MAPBOX_ACCESS_TOKEN = 'pk.eyJ1IjoiY3JpbWVkYXRhc2YiLCJhIjoiY2l2Y296YTl2MDE2bTJ0cGI1NGoyY2RzciJ9.DRX-7gKkJy4FT2Q1Qybb2w';
+    var MAPBOX_MAP_STYLE_ID = 'lightfox.1n10e3dp';
+    var MAP_CONTAINER_ELEMENT_ID = 'map';
+    
+    var SEARCH_MARKER_GEOJSON = {
+        type: 'Feature',
+        geometry: { type: 'Point' },
+        properties: { 'marker-size': 'large' }
+    };
+
+    var INCIDENT_MARKER_PROPERTIES = {
+        'marker-color': '#000080',
+        'marker-symbol': 'police',
+        'marker-size': 'small'
+    };
+
+    var SHAPE_STYLE_SETTINGS = {
+        color: '#0033ff',
+        fillColor: '#0033ff',
+        weight: 5,
+        fillOpacity: 0.2,
+        opacity: 0.5
+    };
+
+    var DRAW_CONTROL_SETTINGS = {
+        draw: {
+            polyline: false,
+            polygon: { shapeOptions: SHAPE_STYLE_SETTINGS },
+            rectangle: { shapeOptions: SHAPE_STYLE_SETTINGS },
+            circle: false,
+            marker: false
+        }
+    };
+
+    var searchAreaGroup = L.featureGroup();
+    var incidentLayer = L.mapbox.featureLayer();
+    var incidentClusterGroup = new L.MarkerClusterGroup({ showCoverageOnHover: false });
+
+    var map;
+
+    function _init() {
+        L.mapbox.accessToken = MAPBOX_ACCESS_TOKEN;
+        map = L.mapbox.map(MAP_CONTAINER_ELEMENT_ID, MAPBOX_MAP_STYLE_ID);
+
+        var drawControl = new L.Control.Draw(DRAW_CONTROL_SETTINGS).addTo(map);
+        searchAreaGroup.addTo(map);
+        incidentLayer.addTo(map);
+        incidentClusterGroup.addTo(map);
+
+        map.on('draw:created', _afterDraw);
+    }
+
+    function _afterDraw(e) {
+        viewModelModule.searchShapeType = 'polygon';
+        viewModelModule.searchGeoJson = e.layer.toGeoJSON();
+        pageModule.loadIncidentData();
+    }
+
+    function _drawPolygonIncidents(incidentGeoJson) {
+        _drawPolygonSearchArea();
+        _drawIncidents(incidentGeoJson);
+    }
+
+    function _drawRadialIncidents(incidentGeoJson) {
+        _drawRadialSearchArea();
+        _drawIncidents(incidentGeoJson);
+    }
+
+    function _drawPolygonSearchArea() {
+        var searchAreaGeoJson = viewModelModule.searchGeoJson;
+        var searchAreaLayer = L.mapbox.featureLayer(searchAreaGeoJson)
+            .setStyle(SHAPE_STYLE_SETTINGS);
+
+        searchAreaGroup.clearLayers()
+            .addLayer(searchAreaLayer);
+    }
+
+    function _drawRadialSearchArea() {
+        var latitude = viewModelModule.latitude,
+            longitude = viewModelModule.longitude,
+            radius = viewModelModule.searchRadius;
+
+        var searchMarkerGeoJson = $.extend(true, {}, SEARCH_MARKER_GEOJSON, {
+            geometry: { coordinates: [ longitude, latitude ] }
+        });
+
+        var searchMarkerLayer = L.mapbox.featureLayer(searchMarkerGeoJson);
+        var searchAreaLayer = L.circle([latitude, longitude], radius);
+
+        searchAreaGroup.clearLayers()
+            .addLayer(searchMarkerLayer)
+            .addLayer(searchAreaLayer);
+    }
+
+    function _drawIncidents(incidentGeoJson) {
+        incidentClusterGroup.clearLayers();
+
+        $.each(incidentGeoJson.features, function(index, feature) {
+            $.extend(feature.properties, INCIDENT_MARKER_PROPERTIES);
+        });
+
+        incidentLayer.setGeoJSON(incidentGeoJson).eachLayer(function(layer) {
+            incidentClusterGroup.addLayer(layer);
+            layer.bindPopup(_buildIncidentPopupContent(layer.feature.properties));
+        });
+
+        incidentLayer.clearLayers();
+        map.fitBounds(searchAreaGroup.getBounds());
+    }
+
+    function _buildIncidentPopupContent(properties) {
+        return properties.descript + '; INCIDENT #: ' + properties.incidntnum;
+    }
+
+    return {
+        init: _init,
+        drawPolygonIncidents: _drawPolygonIncidents,
+        drawRadialIncidents: _drawRadialIncidents
+>>>>>>> dev
     };
 
 })(window, jQuery);
--- js/modules/urlSearch.js
+++ js/modules/urlSearch.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 var urlSearch = (function() {
 
     function _runURLsearch() {
@@ -83,3 +84,53 @@ var urlSearch = (function() {
         getRadius: _getRadius
     }
 })(window, jQuery);
+=======
+var urlSearchModule = (function() {
+
+    var DEFAULT_LATITUDE = 37.768;
+    var DEFAULT_LONGITUDE = -122.438;
+    var DEFAULT_RADIUS = 402.3;
+    var DEFAULT_START_DATE = moment().subtract(29, 'days').format('YYYY-MM-DD');
+    var DEFAULT_END_DATE = moment().format('YYYY-MM-DD');
+    var DEFAULT_SEARCH_ADDRESS = '';
+    var DEFAULT_SHAPE_TYPE = 'radial';
+
+    function _initializeViewModelFromUrlParameters() {
+        var query = new URI().query(true);
+        var params = query.state ? JSON.parse(query.state) : {};
+
+        var addressSearchText =
+            [ params.searchAddress, params.searchCity, params.searchState, params.searchZip ].join(', ');
+
+        addressService.getAddress(addressSearchText, function(address) {
+            var firstFeature = address.features[0];
+            var latitude = firstFeature && firstFeature.coordinates ? firstFeature.coordinates[1] : DEFAULT_LATITUDE;
+            var longitude = firstFeature && firstFeature.coordinates ? firstFeature.coordinates[0] : DEFAULT_LONGITUDE;
+            var radius = params.radius || DEFAULT_RADIUS;
+            var startDate = params.startDate || DEFAULT_START_DATE;
+            var endDate = params.endDate || DEFAULT_END_DATE;
+            var shapeType = params.searchShapeType || DEFAULT_SHAPE_TYPE;
+
+            viewModelModule.latitude = latitude;
+            viewModelModule.longitude = longitude;
+            viewModelModule.searchRadius = radius;
+            viewModelModule.startDate = startDate;
+            viewModelModule.endDate = endDate;
+            viewModelModule.searchAddress = params.searchAddress || null;
+            viewModelModule.searchCity = params.searchCity || null;
+            viewModelModule.searchState = params.searchState || null;
+            viewModelModule.searchZip = params.searchZip || null;
+            viewModelModule.searchShapeType = shapeType;
+            viewModelModule.searchGeoJson = params.searchGeoJson || null;
+
+            $('#inputAddress').val(firstFeature ? firstFeature.properties.label : '');
+
+            pageModule.loadIncidentData();
+        });
+    }
+
+    return {
+        initializeViewModelFromUrlParameters: _initializeViewModelFromUrlParameters
+    };
+})();
+>>>>>>> dev
