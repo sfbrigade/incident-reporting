3a1bca2829ae217f54ac48d5f9b0a6d955b05ed0
Jim Posen <jim.posen@gmail.com>
2017-01-11 23:09:34 -0800

    Move table column config to config file.

--- js/config.js
+++ js/config.js
@@ -6,7 +6,49 @@ var config = (function(window, $) {
             timeColumn: "time",
             dateColumn: "date",
             locationColumn: "location"
-        }
+        },
+        tableColumns: [{
+            data: "incidntnum",
+            title: "Incident#",
+            name: "incidntnum"
+        }, {
+            data: "date",
+            title: "Date",
+            name: "date",
+            render: function(data, type, row, meta) {
+                return moment(data).format('l');
+            },
+            visible: false
+        }, {
+            data: "time",
+            title: "Time",
+            name: "time",
+            visible: false
+        }, {
+            data: "address",
+            title: "Address",
+            name: "address",
+            visible: false
+        }, {
+            data: "pddistrict",
+            title: "District",
+            name: "pddistrict",
+            visible: false
+        }, {
+            className: "mobile",
+            data: "category",
+            title: "Category",
+            name: "category"
+        }, {
+            data: "descript",
+            title: "Description",
+            name: "descript"
+        }, {
+            className: "mobile tablet",
+            data: "resolution",
+            title: "Resolution",
+            name: "resolution"
+        }]
     };
 
 })(window, jQuery);
